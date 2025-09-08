6c0da856d78973ae088982301fe27c02ddec3cbc
VP <reservvp@gmail.com>
2025-09-08 12:45:06 +0300

    Initial Commit

---
+++ Event Hub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Event Hub
+//
+//  Created by VP on 08.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
